f5953195f76640e9cab616a90bd7d0d861816150
Malharii <mrmalhari96@gmail.com>
2025-07-15 14:29:07 +0530

    Complted city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -7,18 +7,44 @@ class CityRepository {
       const city = await City.create(name);
       return city;
     } catch (error) {
+      console.log("somthing went wrong in repostory layer");
       throw { error };
     }
   }
 
   async deleteCity({ cityId }) {
     try {
-      const city = await City.destroy({
+      await City.destroy({
         where: {
           id: cityId,
         },
       });
     } catch (error) {
+      console.log("somthing went wrong in repostory layer");
+      throw { error };
+    }
+  }
+
+  async updateCity(cityId, data) {
+    try {
+      const city = await City.update(data, {
+        where: {
+          id: cityId,
+        },
+      });
+      return city;
+    } catch (error) {
+      console.log("somthing went wrong in repostory layer ");
+      throw { error };
+    }
+  }
+
+  async getCity(cityId) {
+    try {
+      const city = await City.findByPk(cityId);
+      return city;
+    } catch (error) {
+      console.log("somthing went wrong in repostory layer ");
       throw { error };
     }
   }
