f86e669337e80cd02da9ec515236d31a0f7534d2
Malharii <mrmalhari96@gmail.com>
2025-08-06 21:18:05 +0530

    crud repo added

---
+++ src/repository/crud-repository.js
@@ -0,0 +1,67 @@
+const { where } = require("sequelize");
+
+class CrudResitory {
+  constructor(model) {
+    this.model = model;
+  }
+
+  async create(data) {
+    try {
+      await this.model.create(data);
+      return result;
+    } catch (error) {
+      console.log("somothing went wrong in crud repository");
+      throw error;
+    }
+  }
+
+  async destroy(modelId) {
+    try {
+      const result = await this.model.destroy({
+        where: {
+          id: modelId,
+        },
+      });
+      return true;
+    } catch (error) {
+      console.log("somothing went wrong in crud repository");
+      throw error;
+    }
+  }
+
+  async get(modelId) {
+    try {
+      const result = await this.model.findByPk(modelId);
+      return result;
+    } catch (error) {
+      console.log("somothing went wrong in crud repository");
+      throw error;
+    }
+  }
+
+  async getAll() {
+    try {
+      const result = await this.model.findAll();
+      return result;
+    } catch (error) {
+      console.log("somothing went wrong in crud repository");
+      throw error;
+    }
+  }
+
+  async update(modeId, data) {
+    try {
+      const result = await this.model.update(data, {
+        where: {
+          id: modeId,
+        },
+      });
+      return result;
+    } catch (error) {
+      console.log("somothing went wrong in crud repository");
+      throw error;
+    }
+  }
+}
+
+module.exports = CrudResitory;
